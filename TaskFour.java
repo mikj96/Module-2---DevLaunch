a6f8f50ff576a06ea79524b640c3497594d85246
mikj96 <108567951+mikj96@users.noreply.github.com>
2024-07-31 18:40:07 +0200

    Update TaskFour.java

--- TaskFour.java
+++ TaskFour.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class TaskFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int height = sc.nextInt();
+        if (height <= 0) {
+            System.out.println("Triangle height must be larger than 0.");
+            return;
+        }
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
